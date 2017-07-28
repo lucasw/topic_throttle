eded7bfb45ce94900a26d12a6096e15021516cd1
Lucas Walter <wsacul@gmail.com>
2017-07-28 08:15:07 -0700

    I though old messages would be pushed out of queue once it overflows, but it looks like it is the opposite case.

--- scripts/highrate_pub.py
+++ scripts/highrate_pub.py
@@ -9,7 +9,11 @@ rospy.init_node('highrate_pub')
 pub = rospy.Publisher('imu', Imu, queue_size=1)
 rate = rospy.Rate(1000)
 
+count = 0
 imu = Imu()
 while not rospy.is_shutdown():
+    imu.header.stamp = rospy.Time.now()
+    imu.header.frame_id = str(count)
     pub.publish(imu)
+    count += 1
     rate.sleep()
--- scripts/imu_sub.py
+++ scripts/imu_sub.py
@@ -5,18 +5,33 @@ import rospy
 from sensor_msgs.msg import Imu
 
 def callback(data):
+    global last_pub  # rate
+    global start
+    period = rospy.Duration(1.0)
+    elapsed = data.header.stamp - last_pub
+    remaining = period - elapsed
+    rospy.loginfo(str(elapsed.to_sec()) + ' ' + str(period.to_sec()) + ' ' +
+                  str(remaining.to_sec()))
+    rospy.loginfo(data.header.stamp.to_sec() - start.to_sec())
+    rospy.loginfo(last_pub.to_sec() - start.to_sec())
+    rospy.loginfo(rospy.Time.now().to_sec() - start.to_sec())
+    if remaining.to_sec > 0:
+        rospy.sleep(remaining.to_sec())
+        return
+
     global count
     count += 1
     global pub
     pub.publish(data)
-    global rate
-    rate.sleep()
+    last_pub = data.header.stamp
+       # rate.sleep()
 
 rospy.init_node('imu_sub')
 
 # sub = rospy.Subscriber("imu_throttle", Imu, callback)
 count = 0
-rate = rospy.Rate(50)
+last_pub = rospy.Time.now()
+start = rospy.Time.now()
 sub = rospy.Subscriber("imu", Imu, callback, queue_size=3)
 pub = rospy.Publisher("imu_throttle", Imu, queue_size=2)
 rospy.spin()
