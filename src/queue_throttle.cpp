2daed59c320244b774d771f4cc3197af0c6e7d88
Lucas Walter <wsacul@gmail.com>
2017-07-30 08:20:15 -0700

    Allow rate to be set.

--- src/queue_throttle.cpp
+++ src/queue_throttle.cpp
@@ -10,20 +10,29 @@ struct QueueThrottle
   ros::Subscriber sub_;
   ros::Publisher pub_;
   ros::Time last_pub_;
+  ros::Duration period_;
 };
 
 QueueThrottle::QueueThrottle()
 {
   last_pub_ = ros::Time::now();
+  double rate = 0.0;
+  ros::param::get("~rate", rate);
+  if (rate < 0.0)
+  {
+    // TODO(lucasw) maybe rate 0.0 should be no throttling at all?
+    ROS_WARN_STREAM("invalid rate " << rate << ", setting to 1.0");
+    rate = 1.0;
+  }
+  period_ = ros::Duration(1.0 / rate);
   sub_ = nh_.subscribe("topic", 1, &QueueThrottle::callback, this);
 }
 
 void QueueThrottle::callback(const ros::MessageEvent<topic_tools::ShapeShifter>& msg_event)
 {
   ros::Time cur = ros::Time::now();
-  ros::Duration period(1.0);
   ros::Duration elapsed = cur - last_pub_;
-  ros::Duration remaining = period - elapsed;
+  ros::Duration remaining = period_ - elapsed;
   if (remaining.toSec() > 0)
   {
     remaining.sleep();
